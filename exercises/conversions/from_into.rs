b099948c7f8af5c1333abe779890a6f6bedf45df
Manuel Leduc <manuel.leduc@xwiki.com>
2020-11-04 22:30:24 +0100

    up to from_intro

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,23 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let split = s.split(",");
+            let x: Vec<&str> = split.collect();
+            if x.len() == 2 {
+                let name = x[0].to_string();
+                if name.len() == 0 { Person::default() } else {
+                    match x[1].parse::<usize>() {
+                        Ok(age) => Person { name, age },
+                        Err(_) => Person::default()
+                    }
+                }
+            } else { Person::default() }
+        }
     }
 }
 
@@ -52,6 +65,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_default() {
         // Test that the default person is 30 year old John
@@ -59,6 +73,7 @@ mod tests {
         assert_eq!(dp.name, "John");
         assert_eq!(dp.age, 30);
     }
+
     #[test]
     fn test_bad_convert() {
         // Test that John is returned when bad string is provided
@@ -66,6 +81,7 @@ mod tests {
         assert_eq!(p.name, "John");
         assert_eq!(p.age, 30);
     }
+
     #[test]
     fn test_good_convert() {
         // Test that "Mark,20" works
@@ -73,6 +89,7 @@ mod tests {
         assert_eq!(p.name, "Mark");
         assert_eq!(p.age, 20);
     }
+
     #[test]
     fn test_bad_age() {
         // Test that "Mark,twenty" will return the default person due to an error in parsing age
--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -5,9 +5,8 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
-
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
@@ -19,14 +18,15 @@ fn main() {
     let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let status_shared = status.clone();
     thread::spawn(move || {
+        let status_shared = Arc::clone(&status_shared);
         for _ in 0..10 {
-            //thread::sleep(Duration::from_millis(250));
-            let x = status_shared.lock().unwrap();
-            x.jobs_completed += 1;
+            thread::sleep(Duration::from_millis(250));
+            let mut j = status_shared.lock().unwrap();
+            j.jobs_completed += 1;
         }
     });
     while status.lock().unwrap().jobs_completed < 10 {
         println!("waiting... ");
-        //thread::sleep(Duration::from_millis(500));
+        thread::sleep(Duration::from_millis(500));
     }
 }
